ac4efcfe4f301eb5c73e0b7f74c0392c6c8ce745
Sanjog Sharma <sanjogsharma@gmail.com>
2016-12-27 03:28:14 +0000

    time stamp api

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app=express();
 
 
-app.get('/',function(req,res){
+app.get('/:dateinput',function(req,res){
+    var dateinput=req.params.dateinput;
+  
+ dateinput=Number(dateinput) || dateinput;
+   //see if it's natural date
+   var parsed=new Date(dateinput);
+  
+  
+   res.json({unix:isNaN(parsed)?null:parsed.getTime(),
+   natural:isNaN(parsed)?null:parsed.toDateString()});
     
-    res.send('Hellp World');
 })
 
 app.listen(8080, function(){
-    console.log('listening');
+    console.log('listening at 8000');
 })
