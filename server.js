8951506438aef7b86ef3b75ea43f71cfbb6aa716
Sanjog Sharma <sanjogsharma@gmail.com>
2016-12-27 03:38:45 +0000

    added isNaN for date check

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ app.get('/:dateinput',function(req,res){
     var dateinput=req.params.dateinput;
   
  dateinput=Number(dateinput) || dateinput;
-   //see if it's natural date
+   
    var parsed=new Date(dateinput);
   
-  
-   res.json({unix:isNaN(parsed)?null:parsed.getTime(),
-   natural:isNaN(parsed)?null:parsed.toDateString()});
+   res.json({unix:isNaN(parsed.getTime())?null:parsed.getTime(),
+   natural:isNaN(parsed.getTime())?null:parsed.toDateString()});
     
 })
 
