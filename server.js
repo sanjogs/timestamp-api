f80f6eb5d3a742193859d2a858f071dc117334b0
Sanjog Sharma <sanjogsharma@gmail.com>
2016-12-28 01:16:37 +0000

    added index js

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ var express = require('express');
 var app=express();
 
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.get('/',function(req,res){
+    res.render('index');
+});
+
 app.get('/:dateinput',function(req,res){
     var dateinput=req.params.dateinput;
-  
- dateinput=Number(dateinput) || dateinput;
-   
-   var parsed=new Date(dateinput);
-  
-   res.json({unix:isNaN(parsed.getTime())?null:parsed.getTime(),
-   natural:isNaN(parsed.getTime())?null:parsed.toDateString()});
     
+    dateinput=Number(dateinput) || dateinput;
+    
+    var parsed=new Date(dateinput);
+
+   res.json({
+       unix:isNaN(parsed.getTime())?null:parsed.getTime(),
+       natural:isNaN(parsed.getTime())?null:parsed.toDateString()
+   });
 })
 
-app.listen(process.env.PORT || 5000, function(){
+app.listen(process.env.PORT, function(){
     console.log('listening');
 })
